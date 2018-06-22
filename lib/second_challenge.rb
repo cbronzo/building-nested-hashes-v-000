aeb609ace372a4844c8e2ef16a2e073294d2fa2c
Catherine Bronzo <cbronzo@mac.com>
2018-06-22 03:40:54 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,11 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriach => {},
-      
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      
-      }}
+      :patriach => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
 
   
 end
